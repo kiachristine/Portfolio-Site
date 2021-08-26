809faf04a1e7cda6a2832dcdc2a95b5b4166b20d
kiachristine <87938084+kiachristine@users.noreply.github.com>
2021-08-26 11:14:22 -0500

    update index, css and java

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello! Welcome to my Portfolio!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
